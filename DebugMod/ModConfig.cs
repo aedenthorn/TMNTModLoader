a2bfc233ad9927f325a0f02a081235ab61b654c5
Platonymous <david@millionideas.de>
2022-08-06 05:50:40 +0200

    Switches the mod to TMNTModApi & Adds new cheats
    winKey clears the stage, killKey kills all active enemies, lifeKey adds a life, whirlKey starts a whirlwind.

--- DebugMod/ModConfig.cs
+++ DebugMod/ModConfig.cs
@@ -8,6 +8,11 @@ namespace DebugMod
         public string healthKey = "H";
         public string ninjaKey = "N";
         public string switchKey = "OemComma";
+        public string winKey = "M";
+        public string killKey = "K";
+        public string lifeKey = "L";
+        public string whirlKey = "P";
         public bool enableNumKeys = true;
+        public int whirlwindDuration = 5;
     }
 }
